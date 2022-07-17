b32eda959ffebd73211e146e6912aa4191b6edae
Brian Gershon <briangershon@users.noreply.github.com>
2022-07-17 10:32:42 -0700

    “Creating a Pool Instance” tutorial step, plus fix Typescript warning about Address; needed toString.
    Add tsconfig.json to support `resolveJsonModule`
    option.

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,16 @@
+import { PROVIDER_URL } from "./config";
 import { ethers } from "ethers";
 import { Address } from "cluster";
-import { PROVIDER_URL } from "./config";
+import { Pool } from "@uniswap/v3-sdk";
+import { Token } from "@uniswap/sdk-core";
+import { abi as IUniswapV3PoolABI } from "@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json";
 
 const provider = new ethers.providers.JsonRpcProvider(PROVIDER_URL);
-
 const poolAddress = "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8";
-
-const poolImmutablesAbi = [
-  "function factory() external view returns (address)",
-  "function token0() external view returns (address)",
-  "function token1() external view returns (address)",
-  "function fee() external view returns (uint24)",
-  "function tickSpacing() external view returns (int24)",
-  "function maxLiquidityPerTick() external view returns (uint128)",
-];
 
 const poolContract = new ethers.Contract(
   poolAddress,
-  poolImmutablesAbi,
+  IUniswapV3PoolABI,
   provider
 );
 
@@ -30,20 +23,77 @@ interface Immutables {
   maxLiquidityPerTick: number;
 }
 
+interface State {
+  liquidity: ethers.BigNumber;
+  sqrtPriceX96: ethers.BigNumber;
+  tick: number;
+  observationIndex: number;
+  observationCardinality: number;
+  observationCardinalityNext: number;
+  feeProtocol: number;
+  unlocked: boolean;
+}
+
 async function getPoolImmutables() {
-  const PoolImmutables: Immutables = {
-    factory: await poolContract.factory(),
-    token0: await poolContract.token0(),
-    token1: await poolContract.token1(),
-    fee: await poolContract.fee(),
-    tickSpacing: await poolContract.tickSpacing(),
-    maxLiquidityPerTick: await poolContract.maxLiquidityPerTick(),
+  const [factory, token0, token1, fee, tickSpacing, maxLiquidityPerTick] =
+    await Promise.all([
+      poolContract.factory(),
+      poolContract.token0(),
+      poolContract.token1(),
+      poolContract.fee(),
+      poolContract.tickSpacing(),
+      poolContract.maxLiquidityPerTick(),
+    ]);
+
+  const immutables: Immutables = {
+    factory,
+    token0,
+    token1,
+    fee,
+    tickSpacing,
+    maxLiquidityPerTick,
   };
-  return PoolImmutables;
+  return immutables;
+}
+
+async function getPoolState() {
+  const [liquidity, slot] = await Promise.all([
+    poolContract.liquidity(),
+    poolContract.slot0(),
+  ]);
+
+  const PoolState: State = {
+    liquidity,
+    sqrtPriceX96: slot[0],
+    tick: slot[1],
+    observationIndex: slot[2],
+    observationCardinality: slot[3],
+    observationCardinalityNext: slot[4],
+    feeProtocol: slot[5],
+    unlocked: slot[6],
+  };
+
+  return PoolState;
 }
 
 console.log(`Retrieving ${poolAddress} pool info...`);
 
-getPoolImmutables().then((result) => {
-  console.log(result);
-});
+async function main() {
+  const [immutables, state] = await Promise.all([getPoolImmutables(), getPoolState()])
+
+  const TokenA = new Token(3, immutables.token0.toString(), 6, 'USDC', 'USD Coin')
+
+  const TokenB = new Token(3, immutables.token1.toString(), 18, 'WETH', 'Wrapped Ether')
+
+  const poolExample = new Pool(
+    TokenA,
+    TokenB,
+    immutables.fee,
+    state.sqrtPriceX96.toString(),
+    state.liquidity.toString(),
+    state.tick
+  )
+  console.log(poolExample)
+}
+
+main()
